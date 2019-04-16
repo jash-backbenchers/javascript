4179fd39f03978d28f512f5be6eaccaf923338bd
jaswanth chodavarapu <jaswanth.chodavarapu@inmar.com>
2019-04-16 22:53:43 +0530

    initial commit

---
+++ Assignments/Day1/1_Light_bulb/Start/script.js
@@ -0,0 +1,6 @@
+function changeImage() {
+  var image = document.getElementById("myImage");
+  //Bulb image sources
+  // "https://s3-us-west-2.amazonaws.com/s.cdpn.io/93927/pic_bulboff.gif"
+  // "https://s3-us-west-2.amazonaws.com/s.cdpn.io/93927/pic_bulbon.gif"
+}
---
+++ Assignments/Day1/2_Tip_calculator/Start/script.js
@@ -0,0 +1,12 @@
+//Calculate Tip
+function calculateTip() {
+  //validate input
+  //Calculate tip
+  //round to two decimal places
+  //next line allows us to always have two digits after decimal point
+  //Display the tip
+}
+
+//Hide the tip amount on load
+
+//click to call function (on click event listner)
